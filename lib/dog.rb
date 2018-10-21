120bccbe0137752f1ece09ad1d3c0b9b3d7ab5a1
Cara Morelli <caramorelli7@gmail.com>
2018-10-21 21:15:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,7 @@ class Dog
     print row 
     puts 'hello'
     puts ''
-    self.new(id: row[0], name: row[1], breed: row[2])
+    # self.new(id: row[0], name: row[1], breed: row[2])
   end 
   
   def self.find_by_name(name)
