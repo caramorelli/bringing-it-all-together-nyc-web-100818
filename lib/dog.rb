ddcdbd92e6b9926b13e25ac3dd92ed68b8e4163b
Cara Morelli <caramorelli7@gmail.com>
2018-10-21 21:24:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,7 +38,7 @@ class Dog
     question = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
     if question.id.nil?
       info = question[0]
-      new_dog = Dog.new(id: info[0])
+      new_dog = Dog.new(id: info[0], name: )
     self.id.nil? ? self.create(attr_hash) : self.find_by_id(self.id)
   end 
   
