a23484605d139bc38329122699825608a23bf6a7
Cara Morelli <caramorelli7@gmail.com>
2018-10-21 21:26:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,6 +40,7 @@ class Dog
       info = question[0]
       new_dog = Dog.new(id: info[0], name: info[1], breed: info[2])
     else 
+      new_dog = self.create(name: name, breed: breed)
     end 
   end 
   
