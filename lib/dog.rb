77332b861dc15985a020743acdde74d2c10f1a0f
Cara Morelli <caramorelli7@gmail.com>
2018-10-21 20:53:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,8 +57,7 @@ class Dog
       INSERT INTO dogs (name, breed) VALUES( ?, ?)
     SQL
     
-    print DB[:conn].execute(sql, self.name, self.breed)
-    puts ''
+    DB[:conn].execute(sql, self.name, self.breed)
   end 
   
 end 
